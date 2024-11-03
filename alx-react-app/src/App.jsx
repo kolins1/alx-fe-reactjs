06ad57287b7050c6e847399153c16623b79f309f
Collins <ckipkurui77@gmail.com>
2024-11-03 22:22:43 +0300

    first commit

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import WelcomeMessage from './components/WelcomeMessage';
+
+
+function App() {
+  return (
+      <div className="App">
+          <WelcomeMessage />
+      </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
