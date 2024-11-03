087b759525718ebe6abbb6864a36c810f545e254
Collins <ckipkurui77@gmail.com>
2024-11-03 22:52:30 +0300

    first commit

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+
 
 
 function App() {
@@ -11,8 +16,28 @@ function App() {
       </div>
   );
 }
+function App() {
+  return (
+      <div className="App">
+          <Header />
+          <MainContent />
+          <Footer />
+      </div>
+  );
+}
+
+function App() {
+  return (
+      <div className="App">
+          <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+          <UserProfile name="Bob" age="30" bio="Enjoys coding and playing chess" />
+      </div>
+  );
+}
+
 
 export default App;
+
 
 
 
