9f8f2fbeb717c055c38a58cc9a81a7d2dfdc37e3
Collins <ckipkurui77@gmail.com>
2024-11-04 23:56:55 +0300

    first commit

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -5,6 +5,9 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+
+
 
 
 import './App.css'
@@ -14,18 +17,18 @@ function App() {
   return (
       <div className="App">
           <WelcomeMessage />
-      </div>
-  );
-}
-function App() {
-  return (
-      <div className="App">
+          
           <Header />
           <MainContent />
           <Footer />
+          <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+          <UserProfile name="Bob" age="30" bio="Enjoys coding and playing chess" />
       </div>
+        
+      
   );
 }
+
 
 
 
