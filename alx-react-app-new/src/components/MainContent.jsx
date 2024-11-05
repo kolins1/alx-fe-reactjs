f255ec9cf3dc6e2ea8f99df08bed5b02f785412e
Collins <ckipkurui77@gmail.com>
2024-11-05 11:47:41 +0300

    first commit

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 function MainContent() {
     return (
-        <main>
+        <main style={{ padding: '20px', fontSize: '1.2em', color: '#333', lineHeight: '1.6' }}>
             <p>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     );
