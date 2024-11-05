4ade84e067c7aec9bf47b720287a1240d7c84960
Collins <ckipkurui77@gmail.com>
2024-11-05 11:54:31 +0300

    first commit

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -6,6 +6,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 
 
@@ -17,7 +18,7 @@ function App() {
   return (
       <div className="App">
           <WelcomeMessage />
-          
+          <Counter />
           <Header />
           <MainContent />
           <Footer />
